95a010f16eb13a6108b8c2aa47091cf76ca045bf
Joshua Suskin <jsuskin85@gmail.com>
2019-05-09 03:36:35 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def dictionary
+  {
+    "hello": "hi",
+    "to": "2",
+    "two": "2",
+    "too": "2",
+    "for": "4",
+    "four": "4",
+    "be": "b",
+    "you": "u",
+    "at": "@",
+    "and": "&"
+  }
+end
+
+def word_substituter(tweet)
+  arr = tweet.split(" ")
+  arr.each do |word|
+    if dictionary.keys.include?(word)
+      arr[arr.index(word)] = dictionary[word]
+    end
+  end
+  arr.join(" ")
+end
