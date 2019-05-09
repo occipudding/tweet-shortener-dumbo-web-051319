20942dc2b9ed2581629f5382b0308f5779088d3c
Joshua Suskin <jsuskin85@gmail.com>
2019-05-09 03:49:41 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,4 +25,8 @@ def word_substituter(tweet)
   arr.join(" ")
 end
 
-word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
